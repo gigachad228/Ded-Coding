36e986e578167e14665373d417820c64e65dba1e
DedJirniy <109561486+gigachad228@users.noreply.github.com>
2024-08-02 21:40:05 +0300

    discordia bot
    discordia bot

---
+++ newbot.lua
@@ -0,0 +1,106 @@
+local discordia = require('discordia')
+local client = discordia.Client()
+		bo = {
+			--videos
+	        'https://media.discordapp.net/attachments/1079060695183786026/1190933346562363402/video_2023-12-31_00-53-57.mp4?ex=65acd49c&is=659a5f9c&hm=668b61c1e2c3fba10e22e8b01e0ed56f79b04fa171a1cadf5300582c063c8f06&',
+            'https://cdn.discordapp.com/attachments/1200150213369798726/1254799336185790574/blocked.mp4?ex=66ad9032&amp',
+            'https://media.discordapp.net/attachments/1096764785426698353/1268027037151395850/RDT_20240731_043153.mp4?ex=66ad90b4&is=66ac3f34&hm=cef62dc51b18ab0589249d158be487e1b0ced52e8894e9e575ddbc3993fd6f87&',
+            'https://media.discordapp.net/attachments/1096764785426698353/1268027036673511574/RDT_20240731_044503.mp4?ex=66ad90b4&is=66ac3f34&hm=0847afb29b9ce61244dd17e4a855b542561ea3e962619c5448f7089633353572&',
+            'https://media.discordapp.net/attachments/1096764785426698353/1268027037671751831/RDT_20240731_044020.mp4?ex=66ad90b4&is=66ac3f34&hm=b20d551b5dbc94cfa564d2b9f8acee87aeeceb2943eea38eece59f020c7b7981&',
+            'https://cdn.discordapp.com/attachments/1096764785426698353/1221555029237039124/RDT_20240321_092242.mp4?ex=66ad4046&is=66abeec6&hm=edc2fef49eae8ec4aecd0f4c5b1a1e4c54d95982a5daeaf0247ed4507dd9a8d5&',
+            'https://cdn.discordapp.com/attachments/1200150213369798726/1266435021737033748/73c7a036.mp4?ex=66adb4c6&is=66ac6346&hm=4ac20261350fe1e977efe25e81991f8d4963eb5a5b55ba9fb060361f1d45895c&',
+            'https://cdn.discordapp.com/attachments/1200150213369798726/1260288230520000694/bo_sinn_nearly_blocked.mp4?ex=66adc1a1&is=66ac7021&hm=4d16d5032599001fcffabca157c79b9f1f846a67e94a8da928844b344916f202&',
+            'https://cdn.discordapp.com/attachments/1035647221024161905/1261083103355469916/bo_sinn_blocked_comp.mp4?ex=66ae02e9&is=66acb169&hm=9a5c867c9a83a7b3cc15334e1ddc05a6ec36913a5659c2c8cd891c985bd04eec&',
+            --gifs
+            'https://tenor.com/view/bo-sinn-gif-11098444432472051034',
+            'https://tenor.com/view/whitelonia-yarra-money-gif-1576953760545295302',
+            'https://tenor.com/view/bo-sinn-%D0%B1%D0%BE-%D1%81%D0%B8%D0%BD%D0%BD-gif-2806096547326834423',
+            'https://cdn.discordapp.com/attachments/1035647221024161905/1265015993378406555/bo_sinn_obiyasnyaet.gif',
+            'https://cdn.discordapp.com/attachments/1035647221024161905/1265293120161976390/bo_sinn_tantsuet.gif',
+            'https://cdn.discordapp.com/attachments/1200150213369798726/1266434272991121459/bosinnkachaetsya.gif',
+            --images
+            'https://media.discordapp.net/attachments/1035647221024161905/1261629823684575253/cibDsFJbmuY.jpg?ex=66ae05d5&is=66acb455&hm=23dba3a7dd741edd33963b47b1c58d89873949c77add2cedac25b3e0f3ff50b0&=&format=webp&width=793&height=702',
+            'https://media.discordapp.net/attachments/1096764785426698353/1265016760231264439/image.png?ex=66ad292a&is=66abd7aa&hm=47473814fc729f6226c6e3d2a89bed885c6e259cfdd1654eeebe8d5a637c4773&=&format=webp&quality=lossless&width=10000&height=10000',
+            'https://media.discordapp.net/attachments/1096764785426698353/1260286748127133879/image.png?ex=66adc03f&is=66ac6ebf&hm=c5a007050b7e138385bd32935648adc3243c49fc9de7907584a2f0b507d99f2f&=&format=webp&quality=lossless&width=1255&height=645',
+		}
+		me = {
+			            --me
+            'https://cdn.discordapp.com/attachments/1035647221024161905/1262029972567097414/20240713_154500.jpg?ex=66ae2900&is=66acd780&hm=0bb50d4a3b8a0a8304e9aa3e9e328d3913501bdbb8afb325f80f4952d63709d3&',
+            'https://cdn.discordapp.com/attachments/1035647221024161905/1262032314570117211/20240713_154532.mp4?ex=66ae2b2f&is=66acd9af&hm=28faeb5928a0956e4130c1fb06b1849fcfcdb2c898cb404513accd5fcbf80cec&',
+            'https://cdn.discordapp.com/attachments/1035647221024161905/1262032315144605747/20240713_154549.mp4?ex=66ae2b2f&is=66acd9af&hm=162baf57bfa64aff543e06f298a63d263f549b37be4618cae0939e33c879448b&',
+            'https://cdn.discordapp.com/attachments/1035647221024161905/1262032315702579291/20240713_154620.mp4?ex=66ae2b2f&is=66acd9af&hm=fd7794e177523026813af680b334365e3da8f7b284a7d85726416fbb49b612ad&',
+            'https://cdn.discordapp.com/attachments/1200150213369798726/1253828523324342362/20240620_205859.mp4?ex=66adfc8e&is=66acab0e&hm=996c56ea1b97266f114feb3160719aa3c45f09075b3a0028849d518e838c1a74&',
+            'https://cdn.discordapp.com/attachments/1200150213369798726/1253828524108943413/20240620_210351.mp4?ex=66adfc8f&is=66acab0f&hm=763693456810d10bd40d5513fae26c0c64daa5ec41cfc2c9f898d177e234492e&',
+		}
+		fumo = {
+			'⠀  ⣠⠤⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀    ⠀⠀⣀⣀⠀⠀',
+			'⠀⠀⡜⠁⠀⠈⢢⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⠋⠷⠶⠱⡄',
+			'⠀⢸⣸⣿⠀⠀⠀⠙⢦⡀⠀⠀⠀⠀⠀⠀⠀⢀⡴⠫⢀⣖⡃⢀⣸⢹',
+			'⠀⡇⣿⣿⣶⣤⡀⠀⠀⠙⢆⠀⠀⠀⠀⠀⣠⡪⢀⣤⣾⣿⣿⣿⣿⣸',
+			'⠀⡇⠛⠛⠛⢿⣿⣷⣦⣀⠀⣳⣄⠀⢠⣾⠇⣠⣾⣿⣿⣿⣿⣿⣿⣽',
+			'⠀⠯⣠⣠⣤⣤⣤⣭⣭⡽⠿⠾⠞⠛⠷⠧⣾⣿⣿⣯⣿⡛⣽⣿⡿⡼',
+			'⠀⡇⣿⣿⣿⣿⠟⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⠻⣿⣿⣮⡛⢿⠃',
+			'⠀⣧⣛⣭⡾⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢿⣿⣷⣎⡇',
+			'⠀⡸⣿⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⢿⣷⣟⡇',
+			'⣜⣿⣿⡧⠀⠀⠀⠀⠀⡀⠀⠀⠀⠀⠀⠀⣄⠀⠀⠀⠀⠀⣸⣿⡜⡄',
+			'⠉⠉⢹⡇⠀⠀⠀⢀⣞⠡⠀⠀⠀⠀⠀⠀⡝⣦⠀⠀⠀⠀⢿⣿⣿⣹',
+			'⠀⠀⢸⠁⠀⠀⢠⣏⣨⣉⡃⠀⠀⠀⢀⣜⡉⢉⣇⠀⠀⠀⢹⡄⠀⠀',
+			'⠀⠀⡾⠄⠀⠀⢸⣾⢏⡍⡏⠑⠆⠀⢿⣻⣿⣿⣿⠀⠀⢰⠈⡇⠀⠀',
+			'⠀⢰⢇⢀⣆⠀⢸⠙⠾⠽⠃⠀⠀⠀⠘⠿⡿⠟⢹⠀⢀⡎⠀⡇⠀⠀',
+			'⠀⠘⢺⣻⡺⣦⣫⡀⠀⠀⠀⣄⣀⣀⠀⠀⠀⠀⢜⣠⣾⡙⣆⡇⠀⠀',
+			'⠀⠀⠀⠙⢿⡿⡝⠿⢧⡢⣠⣤⣍⣀⣤⡄⢀⣞⣿⡿⣻⣿⠞⠀⠀⠀',
+			'⠀⠀⠀⢠⠏⠄⠐⠀⣼⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠳⢤⣉⢳⠀⠀⠀',
+			'⢀⡠⠖⠉⠀⠀⣠⠇⣿⡿⣿⡿⢹⣿⣿⣿⣿⣧⣠⡀⠀⠈⠉⢢⡀⠀',
+			'⢿⠀⠀⣠⠴⣋⡤⠚⠛⠛⠛⠛⠛⠛⠛⠛⠙⠛⠛⢿⣦⣄⠀⢈⡇⠀',
+			'⠈⢓⣤⣵⣾⠁⣀⣀⠤⣤⣀⠀⠀⠀⠀⢀⡤⠶⠤⢌⡹⠿⠷⠻⢤⡀',
+			'⢰⠋⠈⠉⠘⠋⠁⠀⠀⠈⠙⠳⢄⣀⡴⠉⠀⠀⠀⠀⠙⠂⠀⠀⢀⡇',
+			'⢸⡠⡀⠀⠒⠂⠐⠢⠀⣀⠀⠀⠀⠀⠀⢀⠤⠚⠀⠀⢸⣔⢄⠀⢾⠀',
+			'⠀⠑⠸⢿⠀⠀⠀⠀⢈⡗⠭⣖⡒⠒⢊⣱⠀⠀⠀⠀⢨⠟⠂⠚⠋⠀',
+			'⠀⠀⠀⠘⠦⣄⣀⣠⠞⠀⠀⠀⠈⠉⠉⠀⠳⠤⠤⡤⠞⠀⠀⠀⠀',
+		}
+		pass = {
+			'БезБосиннопасность',
+			'Яразащитунтунтунтун',
+			'СтабильныйCock',
+			'Защитакока',
+			'Cockunblock',
+		}
+		pass1 = math.random(1,5)
+		pass2 = {
+			'ЯГей',
+			'Sucktion',
+			'unblockthatglock',
+			'FuckingCumming',
+			'bigballs'
+		}
+		pass3 = math.random(1,5)
+		pass4 = pass[pass1] .. pass2[pass3]
+client:on('ready', function()
+	print('Logged in as '.. client.user.username)
+end)
+client:on('messageCreate', function(sex)
+	print(pass4)
+	if sex.content == pass4 then
+for i in ipairs(bo) do
+i = i + 1
+sex.channel:send(bo[i])
+print(i)
+	end
+end
+if sex.content == 'bo sinn' or 'Бо синн' then
+	sex.channel:send(bo[math.random(1,18)])
+end
+if sex.content == "fumo" or 'фумо' then
+	for i in ipairs(fumo) do
+		sex.channel:send(fumo[i])
+		i = i + 1
+	end
+end
+if sex.content == pass2[4] then
+	for s in ipairs(me) do
+		s = s + 1
+sex.channel:send(me[s])
+end
+end
+end)
+client:run('Bot MTI2ODc5OTAxMTQ2MTI2NzUzOA.Gci2ac.Cf-Mgl1NpUByRRda1wx0R__sR9faZ_c8kzuPEk')
