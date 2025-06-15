ebbda49d3477f76ba9a0223ca66c0bf9b8bb2dc7
gigachad228 <zakitzakitovich@gmail.com>
2025-06-15 20:57:25 +0300

    funny

--- Greeting/greeting.lua
+++ Greeting/greeting.lua
@@ -1,2 +1,11 @@
 local start = require("mainmod")
+print([[1. sinstart()
+2. ricing()]])
+local func = io.read("*n")
+if func == 1 then
 start.sinstart()
+elseif func == 2 then
+start.rice()
+elseif func > 2 then
+start.sex()
+end
--- Greeting/mainmod.lua
+++ Greeting/mainmod.lua
@@ -1,4 +1,3 @@
-#! /usr/bin/env luajit
 local socket = require("socket")
 -- Пример: ASCII-график функции sin(x)
 local DOSMTH = {}
@@ -7,7 +6,7 @@ for x = 0, 6, 0.2 do
     local y = math.sin(x) * 25 + 100
     local color = "\27[31m"
     local reset = "\27[0m"
-    print(string.rep("/", y) .. color .. string.rep([[/\]], 3) .. reset .. string.rep([[\]], 207-y-3) .. "###")
+    print("##"..string.rep("/", y)..color..string.rep([[/\]], 3)..reset..string.rep([[\]], 201-y).."###")
     end
 end
 function DOSMTH.rice()
@@ -22,7 +21,27 @@ while x > 0 do
     -- Неблокирующая задержка (можно прервать Ctrl+C)
     while socket.gettime() - start_time < 0.03 do end
     start_time = socket.gettime()
-    print(string.rep("/", y) .. color .. string.rep([[/\]], 3) .. reset .. string.rep([[\]],207-y-3) .. "###")
+    print("###"..string.rep("/", y)..color..string.rep([[/\]], 3)..reset..string.rep([[\]],201-y).."###")
 end
+end
+function DOSMTH.sex()
+         os.execute("reset")
+         os.execute("clear")
+         print('cute + funny = cunny')
+         print([[
+
+           FISHKISSFISHKIS               
+       SFISHKISSFISHKISSFISH            F
+    ISHK   ISSFISHKISSFISHKISS         FI
+  SHKISS   FISHKISSFISHKISSFISS       FIS
+HKISSFISHKISSFISHKISSFISHKISSFISH    KISS
+  FISHKISSFISHKISSFISHKISSFISHKISS  FISHK
+      SSFISHKISSFISHKISSFISHKISSFISHKISSF
+  ISHKISSFISHKISSFISHKISSFISHKISSF  ISHKI
+SSFISHKISSFISHKISSFISHKISSFISHKIS    SFIS
+  HKISSFISHKISSFISHKISSFISHKISS       FIS
+    HKISSFISHKISSFISHKISSFISHK         IS
+       SFISHKISSFISHKISSFISH            K
+         ISSFISHKISSFISHK               ]])
 end
 return DOSMTH
